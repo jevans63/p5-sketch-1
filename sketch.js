8d0619cf6729d2af5d7d607a88306b1f5e2c76e2
jevans63 <jevans63@kent.edu>
2021-01-03 13:45:55 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,9 +2,22 @@ function setup()
 {
     createCanvas(600, 600);
     background(0);
+    frameRate(100);
+    let r = random(100);
+    strokeWeight(r*0.005);
 }
   
 function draw() 
 {
-
+    for(let i = 0; i < 1000; i++)
+    {
+        line(i,i+i,i*i,i,i,i*i);
+        stroke(i*i,i,0);
+    }
 }
+
+function mousePressed() 
+{
+    clear();
+    background(0);
+  }
